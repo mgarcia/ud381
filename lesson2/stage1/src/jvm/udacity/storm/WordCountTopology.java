8a6a95522a56ab8edcdcba9ba349a5bc6b3895cb
Marcos Garcia <marcgarcia@groupon.com>
2015-02-23 00:50:23 +0000

    exercise lesson2 stage1

--- lesson2/stage1/src/jvm/udacity/storm/WordCountTopology.java
+++ lesson2/stage1/src/jvm/udacity/storm/WordCountTopology.java
@@ -131,13 +131,18 @@ public class WordCountTopology {
       //increment count.
 
       //Syntax to get the word from the 1st column of incoming tuple
-      //String word = tuple.getString(0);
+      String word = tuple.getString(0);
 
-
+      if (countMap.get(word) == null) {
+        countMap.put(word, 1);
+      }else{
+        Integer val = countMap.get(word);
+        countMap.put(word, ++val);
+      }
 
       //After countMap is updated, emit word and count to output collector
       // Syntax to emit the word and count (uncomment to emit)
-      //collector.emit(new Values(word, countMap.get(word)));
+      collector.emit(new Values(word, countMap.get(word)));
 
       //END YOUR CODE Part 1-of-3
       //***************************************************
@@ -155,7 +160,7 @@ public class WordCountTopology {
       //BEGIN YOUR CODE - Part 2-of-3
       //uncomment line below to declare output
 
-      //outputFieldsDeclarer.declare(new Fields("word","count"));
+      outputFieldsDeclarer.declare(new Fields("word","count"));
 
       //END YOUR CODE - Part 2-of-3
       //****************************************************
@@ -218,6 +223,7 @@ public class WordCountTopology {
     // BEGIN YOUR CODE - Part 3-of-3
 
 
+    builder.setBolt("report-bolt", new ReportBolt(), 1).globalGrouping("count-bolt");
 
     // END YOUR CODE - Part 3-of-3
     //***************************************************
